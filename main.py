888e757837047fe1503f741b028b1688067fc56d
sauxpa <46052865+sauxpa@users.noreply.github.com>
2020-02-16 19:32:14 +0100

    cosmetic change

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from gp_approximation import Kernel
 from bokeh.plotting import figure
 from bokeh.io import curdoc
 from bokeh.models import ColumnDataSource, Panel
-from bokeh.models.widgets import CheckboxGroup, Slider, Tabs
+from bokeh.models.widgets import CheckboxGroup, Slider, Tabs, Div
 from bokeh.layouts import row, WidgetBox
 
 
@@ -16,6 +16,8 @@ def make_dataset(f, kernels_to_plot, n=16, scale=1.0, noise=0.1):
     data = np.linspace(-1, 1, n)
     y = f(data)
 
+    err_text = '<b>Absolute errors:</b><br><ul>'
+    err = dict()
     for kernel_name in kernels_to_plot:
         kernel = Kernel(data=data, y=y, scale=scale, noise=noise, kernel_name=kernel_name)
 
@@ -25,17 +27,30 @@ def make_dataset(f, kernels_to_plot, n=16, scale=1.0, noise=0.1):
             approx_f += weight*fx
         df[kernel_name] = approx_f
 
+        err[kernel_name] = np.max(np.abs(df[kernel_name]-df['f']))
+
+    min_err_kernel = min(err, key = err.get)
+
+    for kernel_name in kernels_to_plot:
+        if kernel_name == min_err_kernel:
+            # highlight the best kernel in bold
+            err_text += '<li><b>{}: {:.4f}</b></li>'.format(kernel_name, err[kernel_name])
+        else:
+            err_text += '<li>{}: {:.4f}</li>'.format(kernel_name, err[kernel_name])
+
+    err_text += '</ul>'
+    div.text = err_text
     # Convert dataframe to column data source
     return ColumnDataSource(df)
 
 
 def make_plot(src):
     # Blank plot with correct labels
-    fig = figure(plot_width = 700,
-                 plot_height = 700,
-                 title = 'GP Approximation',
-                 x_axis_label = 'x',
-                 y_axis_label = 'y'
+    fig = figure(plot_width=700,
+                 plot_height=700,
+                 title='GP Approximation',
+                 x_axis_label='x',
+                 y_axis_label='y'
                 )
 
     # original function
@@ -48,15 +63,15 @@ def make_plot(src):
             )
 
     kernels_to_plot = [kernel_selection.labels[i] for i in kernel_selection.active]
+    colors = ['blue', 'orange', 'green', 'purple']
     for i, kernel_name in enumerate(kernels_to_plot):
-        colors = ['blue', 'orange', 'green', 'purple']
         # GP approximation
         fig.line('plot_points',
          kernel_name,
-         source = src,
-         color = 'color',
+         source=src,
+         color='color',
          legend = 'GP approximation ({})'.format(kernel_name),
-         line_color = colors[i]
+         line_color=colors[i]
         )
 
     fig.legend.click_policy = 'hide'
@@ -120,6 +135,8 @@ def f(x):
 
 initial_kernels = ['gauss', 'exp', 'band', 'sinc']
 
+div = Div(text='<b>Absolute errors:</b><br>', width=200, height=100)
+
 src = make_dataset(f,
                    initial_kernels,
                    n=n_select.value,
@@ -127,13 +144,14 @@ src = make_dataset(f,
                    noise=10**lognoise_select.value,
                    )
 fig = make_plot(src)
-controls = WidgetBox(kernel_selection, n_select, logscale_select, lognoise_select)
+
+controls = WidgetBox(kernel_selection, n_select, logscale_select, lognoise_select, div)
 
 # Create a row layout
 layout = row(controls, fig)
 
 # Make a tab with the layout
-tab = Panel(child=layout, title = 'GP Approximation')
+tab = Panel(child=layout, title='GP Approximation')
 tabs = Tabs(tabs=[tab])
 
 curdoc().add_root(tabs)
