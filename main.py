c254711c3eb61d638013b00b1b5b8327cfe2474c
sauxpa <46052865+sauxpa@users.noreply.github.com>
2020-02-17 07:30:19 +0100

    add brownian tab and refactor for clarity

--- main.py
+++ main.py
@@ -1,85 +1,15 @@
 import numpy as np
-import pandas as pd
-from gp_approximation import Kernel
 
-from bokeh.plotting import figure
 from bokeh.io import curdoc
-from bokeh.models import ColumnDataSource, Panel
+from bokeh.models import Panel
 from bokeh.models.widgets import CheckboxGroup, Slider, Tabs, Div
 from bokeh.layouts import row, WidgetBox
 
-
-def make_dataset(f, kernels_to_plot, n=16, scale=1.0, noise=0.1):
-    xx = np.linspace(-1, 1, 1e3)
-    df = pd.DataFrame({'plot_points': xx, 'f': f(xx)}).set_index('plot_points')
-
-    data = np.linspace(-1, 1, n)
-    y = f(data)
-
-    err_text = '<b>Absolute errors:</b><br><ul>'
-    err = dict()
-    for kernel_name in kernels_to_plot:
-        kernel = Kernel(data=data, y=y, scale=scale, noise=noise, kernel_name=kernel_name)
-
-        approx_f = np.zeros(xx.shape)
-        for weight, func in zip(kernel.weights(), kernel.basis()):
-            fx = func(xx)
-            approx_f += weight*fx
-        df[kernel_name] = approx_f
-
-        err[kernel_name] = np.max(np.abs(df[kernel_name]-df['f']))
-
-    min_err_kernel = min(err, key = err.get)
-
-    for kernel_name in kernels_to_plot:
-        if kernel_name == min_err_kernel:
-            # highlight the best kernel in bold
-            err_text += '<li><b>{}: {:.4f}</b></li>'.format(kernel_name, err[kernel_name])
-        else:
-            err_text += '<li>{}: {:.4f}</li>'.format(kernel_name, err[kernel_name])
-
-    err_text += '</ul>'
-    div.text = err_text
-    # Convert dataframe to column data source
-    return ColumnDataSource(df)
-
-
-def make_plot(src):
-    # Blank plot with correct labels
-    fig = figure(plot_width=700,
-                 plot_height=700,
-                 title='GP Approximation',
-                 x_axis_label='x',
-                 y_axis_label='y'
-                )
-
-    # original function
-    fig.line('plot_points',
-             'f',
-             source = src,
-             color = 'color',
-             legend = 'True function',
-             line_color = 'red'
-            )
-
-    kernels_to_plot = [kernel_selection.labels[i] for i in kernel_selection.active]
-    colors = ['blue', 'orange', 'green', 'purple']
-    for i, kernel_name in enumerate(kernels_to_plot):
-        # GP approximation
-        fig.line('plot_points',
-         kernel_name,
-         source=src,
-         color='color',
-         legend = 'GP approximation ({})'.format(kernel_name),
-         line_color=colors[i]
-        )
-
-    fig.legend.click_policy = 'hide'
-
-    return fig
-
+from render import make_dataset, make_plot
 
 def update(attr, old, new):
+    """Update ColumnDataSource object.
+    """
     # change kernels to plot
     kernels_to_plot = [kernel_selection.labels[i] for i in kernel_selection.active]
 
@@ -89,14 +19,53 @@ def update(attr, old, new):
     noise = 10**lognoise_select.value
 
     # Create new ColumnDataSource
-    new_src = make_dataset(f, kernels_to_plot, n=n, scale=scale, noise=noise)
+    new_src = make_dataset(f,
+                           kernels_to_plot,
+                           n=n,
+                           scale=scale,
+                           noise=noise,
+                           div_=div,
+                           xstart=-1.0,
+                           xend=1.0,
+                           )
 
     # Update the data on the plot
     src.data.update(new_src.data)
 
 
-# select kernel
+def update_bm(attr, old, new):
+    """Update ColumnDataSource object.
+    """
+    # change kernels to plot
+    kernels_to_plot = [kernel_selection_bm.labels[i] for i in kernel_selection_bm.active]
+
+    # Change n to selected value
+    n = n_select_bm.value
+    scale = 10**logscale_select_bm.value
+    noise = 10**lognoise_select_bm.value
+
+    # Create new ColumnDataSource
+    new_src = make_dataset(BM,
+                           kernels_to_plot,
+                           n=n,
+                           scale=scale,
+                           noise=noise,
+                           div_=div_bm,
+                           xstart=0.0,
+                           xend=T,
+                           )
+
+    # Update the data on the plot
+    src_bm.data.update(new_src.data)
+
 available_kernels = ['gauss', 'exp', 'band', 'sinc']
+initial_kernels = ['gauss', 'exp', 'band', 'sinc']
+
+### ABSOLUTE VALUE APPROXIMATION
+def f(x):
+    return np.abs(x)
+
+# select kernel
 kernel_selection = CheckboxGroup(labels=available_kernels, active=list(range(len(available_kernels))))
 
 # Slider to select n
@@ -129,12 +98,6 @@ n_select.on_change('value', update)
 logscale_select.on_change('value', update)
 lognoise_select.on_change('value', update)
 
-# Function to be approximated
-def f(x):
-    return np.abs(x)
-
-initial_kernels = ['gauss', 'exp', 'band', 'sinc']
-
 div = Div(text='<b>Absolute errors:</b><br>', width=200, height=100)
 
 src = make_dataset(f,
@@ -142,8 +105,11 @@ src = make_dataset(f,
                    n=n_select.value,
                    scale=10**logscale_select.value,
                    noise=10**lognoise_select.value,
+                   div_=div,
+                   xstart=-1.0,
+                   xend=1.0,
                    )
-fig = make_plot(src)
+fig = make_plot(src, kernel_selection)
 
 controls = WidgetBox(kernel_selection, n_select, logscale_select, lognoise_select, div)
 
@@ -151,7 +117,73 @@ controls = WidgetBox(kernel_selection, n_select, logscale_select, lognoise_selec
 layout = row(controls, fig)
 
 # Make a tab with the layout
-tab = Panel(child=layout, title='GP Approximation')
-tabs = Tabs(tabs=[tab])
+tab = Panel(child=layout, title='Absolute value')
+
+### BROWNIAN MOTION APPROXIMATION
+T = 1.0
+N = int(1e3)
+tn = np.linspace(0, T, N)
+wn = np.random.randn(N)*np.sqrt(T/N)
+W = np.cumsum(wn)
+
+def BM(x):
+    return np.interp(x, tn, W)
+
+# select kernel
+kernel_selection_bm = CheckboxGroup(labels=available_kernels, active=list(range(len(available_kernels))))
+
+# Slider to select n
+n_select_bm = Slider(start=1,
+                     end=128,
+                     step=1,
+                     value=16,
+                     title='Number of knots'
+                     )
+
+# Slider to select logscale
+logscale_select_bm = Slider(start=-3.0,
+                            end=1.0,
+                            step=0.1,
+                            value=-1.0,
+                            title='Scale (log)'
+                            )
+
+# Slider to select noise
+lognoise_select_bm = Slider(start=-3.0,
+                            end=1.0,
+                            step=0.1,
+                            value=-1.0,
+                            title='Noise (log)'
+                            )
+
+# Update the plot when the value is changed
+kernel_selection_bm.on_change('active', update_bm)
+n_select_bm.on_change('value', update_bm)
+logscale_select_bm.on_change('value', update_bm)
+lognoise_select_bm.on_change('value', update_bm)
+div_bm = Div(text='<b>Absolute errors:</b><br>', width=200, height=100)
+
+src_bm = make_dataset(BM,
+                      initial_kernels,
+                      n=n_select_bm.value,
+                      scale=10**logscale_select_bm.value,
+                      noise=10**lognoise_select_bm.value,
+                      # which='bm',
+                      div_=div_bm,
+                      xstart=0.0,
+                      xend=T,
+                   )
+fig_bm = make_plot(src_bm, kernel_selection)
+
+controls_bm = WidgetBox(kernel_selection_bm, n_select_bm, logscale_select_bm, lognoise_select_bm, div_bm)
+
+# Create a row layout
+layout = row(controls_bm, fig_bm)
+
+# Make a tab with the layout
+tab_bm = Panel(child=layout, title='Brownian motion')
+
+### ALL TABS TOGETHER
+tabs = Tabs(tabs=[tab, tab_bm])
 
 curdoc().add_root(tabs)
---
+++ render.py
@@ -0,0 +1,88 @@
+import numpy as np
+import pandas as pd
+from gp_approximation import Kernel
+from bokeh.plotting import figure
+from bokeh.models import ColumnDataSource
+
+
+def make_dataset(f,
+                 kernels_to_plot,
+                 n=16,
+                 scale=1.0,
+                 noise=0.1,
+                 div_=None,
+                 xstart=-1.0,
+                 xend=1.0,
+                 ):
+    """Creates a ColumnDataSource object with data to plot.
+    """
+    xx = np.linspace(xstart, xend, 1e3)
+    df = pd.DataFrame({'plot_points': xx, 'f': f(xx)}).set_index('plot_points')
+
+    data = np.linspace(xstart, xend, n)
+    y = f(data)
+
+    err_text = '<b>Absolute errors:</b><br><ul>'
+    err = dict()
+    for kernel_name in kernels_to_plot:
+        kernel = Kernel(data=data, y=y, scale=scale, noise=noise, kernel_name=kernel_name)
+
+        approx_f = np.zeros(xx.shape)
+        for weight, func in zip(kernel.weights(), kernel.basis()):
+            fx = func(xx)
+            approx_f += weight*fx
+        df[kernel_name] = approx_f
+
+        err[kernel_name] = np.max(np.abs(df[kernel_name]-df['f']))
+
+    min_err_kernel = min(err, key = err.get)
+
+    for kernel_name in kernels_to_plot:
+        if kernel_name == min_err_kernel:
+            # highlight the best kernel in bold
+            err_text += '<li><b>{}: {:.4f}</b></li>'.format(kernel_name, err[kernel_name])
+        else:
+            err_text += '<li>{}: {:.4f}</li>'.format(kernel_name, err[kernel_name])
+
+    err_text += '</ul>'
+    div_.text = err_text
+
+    # Convert dataframe to column data source
+    return ColumnDataSource(df)
+
+
+def make_plot(src, kernel_selection_):
+    """Create a figure object to host the plot.
+    """
+    # Blank plot with correct labels
+    fig = figure(plot_width=700,
+                 plot_height=700,
+                 title='GP Approximation',
+                 x_axis_label='x',
+                 y_axis_label='y'
+                )
+
+    # original function
+    fig.line('plot_points',
+             'f',
+             source = src,
+             color = 'color',
+             legend = 'True function',
+             line_color = 'red'
+            )
+
+    kernels_to_plot = [kernel_selection_.labels[i] for i in kernel_selection_.active]
+    colors = ['blue', 'orange', 'green', 'purple']
+    for i, kernel_name in enumerate(kernels_to_plot):
+        # GP approximation
+        fig.line('plot_points',
+         kernel_name,
+         source=src,
+         color='color',
+         legend = 'GP approximation ({})'.format(kernel_name),
+         line_color=colors[i]
+        )
+
+    fig.legend.click_policy = 'hide'
+
+    return fig
